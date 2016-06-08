964e9124f8d40aee737c6d4b26afad3d50884201
hlxdev <841554024@qq.com>
2016-06-08 09:57:49 +0800

    项目初步建立

---
+++ EaseMobDemo/EaseMobDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EaseMobDemo
+//
+//  Created by hlxdev on 16/6/8.
+//  Copyright © 2016年 hlxdev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ EaseMobDemo/EaseMobDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EaseMobDemo
+//
+//  Created by hlxdev on 16/6/8.
+//  Copyright © 2016年 hlxdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
